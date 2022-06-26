4ed5346593b42c9d664de871516c7466827612f7
James Butcher <jamesbutcher167@gmail.com>
2022-06-26 12:08:47 +0100

    Update LICENSE infomation

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject terra-server "0.1.0-SNAPSHOT"
-  :description "An alternative terraria server written in clojure "
+  :description "An alternative terraria server written in clojure"
   :url "https://github.com/KJ002/terra-server"
-  :license {:name "MIT"}
+  :license {:name "EPL-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns terra-server.core})
