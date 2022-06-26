77301afb6535a72810393ecd4a4bbe3e6f2043d0
James Butcher <jamesbutcher167@gmail.com>
2022-06-26 18:20:03 +0100

    Include dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,10 @@
   :url "https://github.com/KJ002/terra-server"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repl-options {:init-ns terra-server.core})
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [aleph "0.5.0-rc2"]
+                 [byte-streams "0.2.5-alpha2"]
+                 [manifold "0.2.4"]
+                 [gloss "0.2.6"]]
+  :repl-options {:init-ns terra-server.core}
+  :main terra-server.core)
