a185858ecdbfaa42dea1a22d4965ba33aa485600
James Butcher <jamesbutcher167@gmail.com>
2022-06-26 12:01:54 +0100

    Create lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to terra-server
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject terra-server "0.1.0-SNAPSHOT"
+  :description "An alternative terraria server written in clojure "
+  :url "https://github.com/KJ002/terra-server"
+  :license {:name "MIT"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns terra-server.core})
