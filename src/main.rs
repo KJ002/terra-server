2aad8d8c71cc3654247a12905328e77c051367c5
James Butcher <jamesbutcher167@gmail.com>
2022-06-29 11:12:33 +0100

    Read incoming traffic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use tokio;
+use tokio::net::TcpListener;
+use tokio::net::TcpStream;
+
+use std::io;
+
+async fn process_socket(socket: TcpStream) -> io::Result<()> {
+    socket.readable().await?;
+
+    let mut buffer: [u8; 1024] = [0; 1024];
+    let bytes_read = socket.try_read(&mut buffer)?;
+
+    println!("{:?}", &buffer[..=bytes_read]);
+
+    socket.writable().await?;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:2345").await?;
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+        process_socket(socket).await?;
+    }
 }
